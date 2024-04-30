43e4ab680c4349592685aa9c2a5a25842e0ff14b
br1ckw0rm <br1ckw0rm@protonmail.com>
2024-04-30 09:37:43 -0700

    feat: Add zshrc and plugins
    - Adds zshrc
    - Updates install.sh to include symlinking zshrc to ~/.zshrc
    - Updates install.sh to include installing the two plugins defined in
      zshrc

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ ln -s $(pwd)/scripts/yt ~/bin/yt
 # Setup hyprland with my preferred setup
 bash <(curl -s "https://end-4.github.io/dots-hyprland-wiki/setup.sh")
 
+# Install syntax highlighting, and auto suggestions for zsh
+# As these two plugs are defined in our zshrc plugin list.
+sudo pacman -Syu zsh-syntax-highlighting zsh-autosuggestions
+ln -s $(pwd)/zsh/zshrc ~/.zshrc
+
