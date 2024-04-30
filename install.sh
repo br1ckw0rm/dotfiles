a10ed02701f6ebe2d1ad2129cff99bb6535cf0d8
br1ckw0rm <br1ckw0rm@protonmail.com>
2024-04-30 09:30:26 -0700

    fix: Path issue in install.sh script
    - Fixes the path issue for the location of the vimrc file
    - Adds the yt script path

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 
 # create a synlink for ~/.vimrc
-ln -s $(pwd)/vimrc ~/.vimrc 
+ln -s $(pwd)/vim/vimrc ~/.vimrc 
+ln -s $(pwd)/scripts/yt ~/bin/yt
 
 # Setup hyprland with my preferred setup
 bash <(curl -s "https://end-4.github.io/dots-hyprland-wiki/setup.sh")
